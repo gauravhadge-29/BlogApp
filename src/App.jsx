d827635890a5fb331b7550922ef3c14ccb65f69c
gauravhadge-29 <gauravhadge.official@gmail.com>
2025-06-24 17:17:57 +0530

    tailwind setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,43 @@
-
+import { useDispatch } from 'react-redux'
+import { useState, useEffect } from 'react'
 import './App.css'
+import authService from './appwrite/auth'
+import {login,logout} from './store/authSlice'
+import {Header,Footer} from './components/index'
+import { Outlet } from 'react-router-dom'
 
 function App() {
 
+  const [loading, setLoading] = useState(true);
+  const dispatch = useDispatch()
 
-  return (
-    <>
-      <h1>Blog App with Appwrite {import.meta.env.VITE_APPWRITE_URL}</h1>
-    </>
+  useEffect(() => {
+    authService.getCurrentUser()
+      .then((userData) => {
+        if (userData) {
+          dispatch(login({userData}))
+        }else{
+          dispatch(logout())
+        }
+      })
+      .finally(()=>{
+        setLoading(false)
+      })
+  }, [])
+
+  return !loading ? (
+    <div className='min-h-screen flex flex-wrap content-between bg-gray-400'>
+      <div className='w-full block'>
+        <Header/>
+        <main>
+          <Outlet/> 
+          <div>Hello</div>
+        </main>
+        <Footer/>
+      </div>
+    </div>
+  ) : (
+    <div>Loading</div>
   )
 }
 
