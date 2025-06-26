b9fc924dc1d09b555e520df0c74f5447629482f5
gauravhadge-29 <gauravhadge.official@gmail.com>
2025-06-26 14:22:40 +0530

    production grade react components

--- src/components/index.js
+++ src/components/index.js
@@ -1,7 +1,13 @@
 import Header from "./Header/Header";
 import Footer from "./Footer/Footer";
+import Container from "./container/Container";
+import LogoutButton from "./Header/LogoutButton";
+import Logo from "./Logo";
 
 export {
     Header,
-    Footer
+    Footer,
+    Container,
+    LogoutButton,
+    Logo
 }
